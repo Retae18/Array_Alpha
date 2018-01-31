68a9f353b81e3bf043b7b13b33a4d7321c68015e
Retae18 <35357413+Retae18@users.noreply.github.com>
2018-01-31 14:37:30 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_VowTrue
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            /*Write a function that receives a character and returns true if the 
+              character is a vowel and false otherwise.*/
+
+            char[] vow = { 'A', 'E', 'I', 'O', 'U' };       //set an array, remember flower brackets
+            vow[0] = 'A';
+            vow[1] = 'E';
+            vow[2] = 'I';
+            vow[3] = 'O';
+            vow[4] = 'U';
+
+            Console.WriteLine("Enter Character");
+            char x = Convert.ToChar(Console.ReadLine());        //converting array to unicode 
+
+            Console.WriteLine(isVow(x));        //call method
+
+            Console.Read();                     //allow visiual of input and results 
+        }
+
+        static bool isVow (char x)
+        {
+            if (x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U')       //method behavior 
+            {
+                return true;
+            }
+
+            else
+                return false;
+        }
+        
+    } 
+}
+
